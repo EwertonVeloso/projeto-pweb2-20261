6caa6321d48dfef05c4a30b7acfebc97fc83a967
EwertonVeloso <velosoewerton@gmail.com>
2026-06-17 22:39:29 -0300

    adiciona animação nas transações

--- financas-webapp/src/components/transactions/TransactionListContent.tsx
+++ financas-webapp/src/components/transactions/TransactionListContent.tsx
@@ -68,7 +68,7 @@ export default function TransactionListContent() {
   };
 
   return (
-    <div className="p-6 max-w-6xl mx-auto">
+    <div className="p-6 max-w-6xl mx-auto animate-surgir">
       {/* Cabeçalho */}
       <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 mb-6">
         <div>
